6ddd441774dda9675ebeb4b399b7f29c41923af2
josval56 <josval56@gmail.com>
2020-11-10 04:53:00 -0300

    Initial commit

---
+++ src/moneda/Moneda.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package moneda;
+
+/**
+ *
+ * @author Valmore
+ */
+public class Moneda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Cotizador ventana = new Cotizador();
+         ventana.setVisible(true);
+    }
+    
+}
